9ff38744ea7cfc87017ae18661fedd3338abe49a
María Emilia Landa <emilia.landa@gmail.com>
2023-07-01 14:08:39 -0300

    completed app-unresponsive

--- my-react-app/src/components/Contact/Contact.jsx
+++ my-react-app/src/components/Contact/Contact.jsx
@@ -1,50 +1,54 @@
 import React from 'react';
 import styles from './contact.module.css';
-import logo from '../../assets/Group 45.png'
+import logo from '../../assets/Group 45.png';
+import { FiMapPin } from 'react-icons/fi';
+import { FaPhone, FaFacebookSquare, FaInstagram, FaTwitterSquare } from 'react-icons/fa';
 
 function Contact() {
   return (
     <div className={styles.container}>
-     <img src={logo} alt="Logo"  className={styles.logo} />
-      <p>A Better Weigh Medical Weight Loss Center offers a weight loss program dedicated to helping you achieve long term weight loss success with the use of FDA approved medication.</p>
-      <div className={styles.info}>
-        <div className={styles.iconContainer}>
-          <span className="material-icons">location_on</span>
+      <div className={styles.block3}>
+        <img src={logo} alt="Logo" className={styles.logo} />
+        <div className={styles.text}>
+          <p>A Better Weigh Medical Weight Loss Center offers a weight loss program dedicated to helping you achieve long term weight loss success with the use of FDA approved medication.</p>
         </div>
-        <p>Head Office: 1931 W 95th Streeet Chicago IL, 60643</p>
-      </div>
-      <div className={styles.info}>
-        <div className={styles.iconContainer}>
-          <span className="material-icons">phone</span>
+        <div className={styles.info}>
+          <p><FiMapPin className={styles.pinpoint} /><strong>Head Office: </strong>1931 W 95th Streeet Chicago IL, 60643</p>
         </div>
-        <p>Phone: 773-496-4222</p>
-      </div>
-      <div className={styles.socialMedia}>
-        <div className={styles.iconContainer}>
-          <span className="material-icons">facebook</span>
+        <div className={styles.info}>
+          <p><FaPhone className={styles.phone} /><strong>Phone: </strong>773-496-4222</p>
         </div>
-        <div className={styles.iconContainer}>
-          <span className="material-icons">instagram</span>
-        </div>
-        <div className={styles.iconContainer}>
-          <span className="material-icons">twitter</span>
+        <div className={styles.socialMedia}>
+          <span className={styles.FaFacebookSquare}><FaFacebookSquare /></span>
+          <span className={styles.instagram}><FaInstagram /></span>
+          <span className={styles.twitter}><FaTwitterSquare /></span>
         </div>
       </div>
-      <h3>About</h3>
-      <ul>
-        <li>About Us</li>
-        <li>Locations</li>
-        <li>Real Patient Stories</li>
-        <li>Weight Loss Programs</li>
-      </ul>
-      <h3>More Links</h3>
-      <ul>
-        <li>Franchise Opportunities</li>
-        <li>Career Opportunities</li>
-        <li>Terms & Conditions</li>
-        <li>Return Policy</li>
-      </ul>
-      <h5>Copyright by A Better Weigh Inc. All rights reserved.</h5>
+      <div className={styles.block4}>
+        <div className={styles.listBlocks}>
+          <div>
+            <h3 className={styles.listTitles}>About</h3>
+            <ul className={styles.list}>
+              <li className={styles.item}><a href="/about">About Us</a></li>
+              <li className={styles.item}><a href="/locations">Locations</a></li>
+              <li className={styles.item}><a href="/stories">Real Patient Stories</a></li>
+              <li className={styles.item}><a href="/programs">Weight Loss Programs</a></li>
+            </ul>
+          </div>
+          <div>
+            <h3 className={styles.listTitles}>More Links</h3>
+            <ul className={styles.list}>
+              <li className={styles.item}><a href="/franchise">Franchise Opportunities</a></li>
+              <li className={styles.item}><a href="/careers">Career Opportunities</a></li>
+              <li className={styles.item}><a href="/terms">Terms & Conditions</a></li>
+              <li className={styles.item}><a href="/returns">Return Policy</a></li>
+            </ul>
+          </div>
+        </div>
+        <div className={styles.footer}>
+        <h5 className={styles.footer}>Copyright by A Better Weigh Inc. All rights reserved.</h5>
+        </div>
+      </div>
     </div>
   );
 }
--- my-react-app/src/components/FAQ/FAQ.jsx
+++ my-react-app/src/components/FAQ/FAQ.jsx
@@ -18,107 +18,131 @@ function FAQ() {
     <div className={styles.container}>
       <h2 className={styles.faqTitle}>Frequently Asked Questions</h2>
       <div className={styles.accordion}>
+        <div className={styles.QA}>
         <h4 className={styles.title} onClick={() => toggleAccordion(0)}>
           Why is it free?
-         { activeIndex === 0 ? <RiArrowUpSLine/> : <RiArrowDownSLine/>}
+         { activeIndex === 0 ? <RiArrowUpSLine className={styles.arrowUp}/> : <RiArrowDownSLine className={styles.arrowDown}/>}
         </h4>
         {activeIndex === 0 && (
           <p className={styles.content}>
             Answer 1
           </p>
         )}
+        </div>
+        <div className={styles.QA}>
         <h4 className={styles.title} onClick={() => toggleAccordion(1)}>
           Do I need this if I’ve had my yearly medical check? What is the difference?
-          { activeIndex === 0 ? <RiArrowUpSLine/> : <RiArrowDownSLine/>}
+          { activeIndex === 0 ? <RiArrowUpSLine className={styles.arrowUp}/> : <RiArrowDownSLine className={styles.arrowDown}/>}
         </h4>
         {activeIndex === 1 && (
           <p className={styles.content}>
             Answer 2
           </p>
         )}
+        </div>
+        <div className={styles.QA}>
         <h4 className={styles.title} onClick={() => toggleAccordion(2)}>
           Can I bring another person to the appointment?
-          { activeIndex === 0 ? <RiArrowUpSLine/> : <RiArrowDownSLine/>}
+          { activeIndex === 0 ? <RiArrowUpSLine className={styles.arrowUp}/> : <RiArrowDownSLine className={styles.arrowDown}/>}
         </h4>
         {activeIndex === 2 && (
           <p className={styles.content}>
             Answer 3
           </p>
         )}
+        </div>
+        <div className={styles.QA}>
         <h4 className={styles.title} onClick={() => toggleAccordion(3)}>
           How long does it take?
-          { activeIndex === 0 ? <RiArrowUpSLine/> : <RiArrowDownSLine/>}
+          { activeIndex === 0 ? <RiArrowUpSLine className={styles.arrowUp}/> : <RiArrowDownSLine className={styles.arrowDown}/>}
         </h4>
         {activeIndex === 3 && (
           <p className={styles.content}>
             Answer 4
           </p>
         )}
+        </div>
+        <div className={styles.QA}>
         <h4 className={styles.title} onClick={() => toggleAccordion(4)}>
           What if I have to cancel or if I’m not sure I’ll be able to make it?
-          { activeIndex === 0 ? <RiArrowUpSLine/> : <RiArrowDownSLine/>}
+          { activeIndex === 0 ? <RiArrowUpSLine className={styles.arrowUp}/> : <RiArrowDownSLine className={styles.arrowDown}/>}
         </h4>
         {activeIndex === 4 && (
           <p className={styles.content}>
             Answer 5
           </p>
         )}
+        </div>
+        <div className={styles.QA}>
         <h4 className={styles.title} onClick={() => toggleAccordion(5)}>
           Do I need to bring anything with me?
-          { activeIndex === 0 ? <RiArrowUpSLine/> : <RiArrowDownSLine/>}
+          { activeIndex === 0 ? <RiArrowUpSLine className={styles.arrowUp}/> : <RiArrowDownSLine className={styles.arrowDown}/>}
         </h4>
         {activeIndex === 5 && (
           <p className={styles.content}>
             Answer 6
           </p>
         )}
+        </div>
+        <div className={styles.QA}>
         <h4 className={styles.title} onClick={() => toggleAccordion(6)}>
           What if I don’t have insurance?
-          { activeIndex === 0 ? <RiArrowUpSLine/> : <RiArrowDownSLine/>}
+          { activeIndex === 0 ? <RiArrowUpSLine className={styles.arrowUp}/> : <RiArrowDownSLine className={styles.arrowDown}/>}
         </h4>
         {activeIndex === 6 && (
           <p className={styles.content}>
             Answer 7
           </p>
         )}
+        </div>
+        <div className={styles.QA}>
         <h4 className={styles.title} onClick={() => toggleAccordion(7)}>
           Do the weight loss programs you offer work?
-          { activeIndex === 0 ? <RiArrowUpSLine/> : <RiArrowDownSLine/>}
+          { activeIndex === 0 ? <RiArrowUpSLine className={styles.arrowUp}/> : <RiArrowDownSLine className={styles.arrowDown}/>}
         </h4>
         {activeIndex === 7 && (
           <p className={styles.content}>
             Answer 8
           </p>
         )}
+        </div>
+        <div className={styles.QA}>
         <h4 className={styles.title} onClick={() => toggleAccordion(8)}>
           What's the catch?
-          { activeIndex === 0 ? <RiArrowUpSLine/> : <RiArrowDownSLine/>}
+          { activeIndex === 0 ? <RiArrowUpSLine className={styles.arrowUp}/> : <RiArrowDownSLine className={styles.arrowDown}/>}
         </h4>
         {activeIndex === 8 && (
           <p className={styles.content}>
             Answer 9
           </p>
         )}
+        </div>
+        <div className={styles.QA}>
         <h4 className={styles.title} onClick={() => toggleAccordion(9)}>
           Will I have to exhaust myself in the gym to make it work?
-          { activeIndex === 0 ? <RiArrowUpSLine/> : <RiArrowDownSLine/>}
+          { activeIndex === 0 ? <RiArrowUpSLine className={styles.arrowUp}/> : <RiArrowDownSLine className={styles.arrowDown}/>}
         </h4>
         {activeIndex === 9 && (
           <p className={styles.content}>
             Answer 10
           </p>
         )}
+        </div>
+        <div className={styles.QA}>
         <h4 className={styles.title} onClick={() => toggleAccordion(10)}>
           Where are you located?
-          { activeIndex === 0 ? <RiArrowUpSLine/> : <RiArrowDownSLine/>}
+          { activeIndex === 0 ? <RiArrowUpSLine className={styles.arrowUp}/> : <RiArrowDownSLine className={styles.arrowDown}/>}
         </h4>
         {activeIndex === 10 && (
           <p className={styles.content}>
             Answer 11
           </p>
         )}
+        </div>
       </div>
-      <BookButton />
+       <div className={styles.BookBtn}>
+      <button className={styles.BookButton}>Book my appointment</button>
+      </div>
     </div>
   );
 }
